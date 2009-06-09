f5445df2455dc0c3d05cc1c5dbc52ae245e82039
fgochez <fgochez@mango-solutions.com>
2009-06-09 09:25:49 +0000

    added function to handle production of grouping variable that handles "iVar" in a special manner (incomplete)
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@7521 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/graphutils.R
+++ R/graphutils.R
@@ -21,3 +21,22 @@ varComboMatrix <- function(xVars, yVars, collapseY = TRUE, collapseX = FALSE)
 	if(collapseX) xVars <- paste(xVars, collapse = "+")
 	as.matrix(expand.grid(xVars, yVars))
 }
+# TODO: this does not seem to work correctly when group is not NULL
+subjectGrouping <- function(idLabels, group = NULL, superposeCol)
+{
+	if(!is.null(group))
+	{
+		grouping <- paste(group, idLabels, sep = ",")
+		groupTable <- cbind(idLabels, group)
+		indices <- match(unique(idLabels), idLabels)
+		groupTable <- groupTable[indices,]
+		cols <- rep(superposeCol, length.out = length(unique(grouping)))
+		cols <- cols[groupTable[,2]]
+	}
+	else
+	{
+		grouping <- idLabels
+		cols <- superposeCol[1]
+	}
+	return(list(grouping = grouping, colours = cols))
+}
