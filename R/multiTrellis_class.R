c862a5a26c9afdfc37d7d550a13e684fffdb78a5
fgochez <fgochez@mango-solutions.com>
2009-06-11 08:16:59 +0000

    maxPanels was not being passed correctly
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@7649 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/multiTrellis_class.R
+++ R/multiTrellis_class.R
@@ -46,7 +46,7 @@ plotSingletonmultiTrellis <- function(object)
 		object@plots[[1]]$as.table <- as.table
 	if(length(object@maxPanels) > 0)
 	{
-		object@plots[[1]] <- calcMaxPanels(object@plots[[1]], maxPanels)
+		object@plots[[1]] <- calcMaxPanels(object@plots[[1]], object@maxPanels)
 	}
 	print(object@plots[[1]])
 }
