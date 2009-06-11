820b352135604b3917dc653e5160846d27bf8611
fgochez <fgochez@mango-solutions.com>
2009-06-11 16:37:43 +0000

    new set of tools for dealing with scatter plots, starting with a function to generate legends
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@7684 4c17f647-02f2-7e4c-97ba-2b8bca437fff

---
+++ R/scatterplottools.R
@@ -0,0 +1,53 @@
+# TODO: Add comment
+# 
+# Author: fgochez
+###############################################################################
+
+
+scatterPlotKey <- function(gVar, gVarValues, type = c("p", "i", "t", "l", "o"))
+{
+	graphParams <- getAllGraphParams()
+	gVarLevels <- unique(gVarValues)
+	numLevels <- length(gVarLevels)
+	RNMGraphicsStopifnot(numLevels > 0)
+	# list(title = "Variable", cex=.7, rows = 10, space = "right")
+	type <- match.arg(type)
+	# handle those that don't use text first
+	if(type %in% c("p", "l", "o")) {
+		points <- list()
+		
+		x <-with(graphParams, { 
+				pointCol <- rep(superpose.symbol$col, length.out = numLevels)
+				pointPch <- rep(superpose.symbol$pch, length.out = numLevels)
+				lineCol <- rep(superpose.line$col, length.out = numLevels)
+				lineStyle <- rep(superpose.line$lty, length.out = numLevels)
+				
+				switch( type,
+					"p" = {
+						list(points = list(col = pointCol, pch = pointPch))
+					},
+					"o" = {
+						list(points = list(col = pointCol, pch = pointPch), 
+								lines = list(col = lineCol, lty = lineStyle))
+					},
+					"l" = {
+						list(lines = list(col = lineCol, lty = lineStyle))
+					})
+			}
+		)
+		x$rows <- 10
+		x$cex <- 0.7
+		x$space <- "right"	
+		x$title <- gVar
+		# x$text <- list(as.character(sort(gVarLevels)))
+		result <- c(list(text = list(as.character(sort(gVarLevels)))), x)
+	}
+	else
+	{
+		result <- list(space = "right", text =  list(as.character(sort(gVarLevels)), 
+						col = getGraphParams("superpose.text")$col[1:length(gVarLevels)] ),
+				
+			title = gVar, cex = 0.7, rows = 10) 
+	}
+	result
+}
