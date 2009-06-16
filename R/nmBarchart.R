26818b886ec11dba3349e40baa9a6874d5fbe41a
fgochez <fgochez@mango-solutions.com>
2009-06-16 15:17:33 +0000

    added method for NMBasicProblem, capturing variable labels in legend as well (related to Mantis PopPKPD ticket 0000399 ).
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@7799 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/nmBarchart.R
+++ R/nmBarchart.R
@@ -65,7 +65,8 @@ nmBarChart.data.frame <- function(obj, xVars, yVars, xLab = NULL, yLab = NULL, t
 		tab <- table(obj[[currentX]], obj[[currentY]])
 		# tab[,1] <- tab[,1] / sum(tab[,1]); tab[,2] <- tab[,2]
 		tab <- sweep(tab, 1, rowSums(tab), "/")
-		if(addLegend) key <- list(points = FALSE, rectangles = TRUE, space = "right")
+		if(addLegend) key <- list(title = getVarLabel(currentY), 
+					points = FALSE, rectangles = TRUE, space = "right", cex = 0.7)
 		else key <- NULL
 		plotList[[i]] <- 
 			with(graphParams, 	
@@ -82,3 +83,4 @@ nmBarChart.data.frame <- function(obj, xVars, yVars, xLab = NULL, yLab = NULL, t
 }
 
 setMethod("nmBarChart", signature(obj = "data.frame"), nmBarChart.data.frame)
+setMethod("nmBarChart", signature(obj = "NMBasicModel"), nmBarChart.NMBasicModel)
